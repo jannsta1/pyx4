528273503285db379e7b7551e3123cd10b6bda82
Joan Reyero <joanreyero@gmail.com>
2020-06-29 12:37:49 +0100

    Added the possibility to set a maximum speed for linear and angular velocities.

--- src/pyx4_base/teleoperation.py
+++ src/pyx4_base/teleoperation.py
@@ -3,7 +3,7 @@
 import argparse
 import os, sys
 import rospy
-
+import numpy as np
 
 from generate_mission import Wpts_from_csv
 from definitions_pyx4 import MISSION_SPECS
@@ -23,6 +23,8 @@ class Teleop_state(Generic_mission_state):
                  # waypoint state labels are mandatory
                  state_label='generic teleop',
                  timeout=60,
+                 max_linear_speed=4,
+                 max_angular_speed=2,
                  mavros_message_node=None,
                  parent_ref=None,
                  **kwargs
@@ -42,6 +44,8 @@ class Teleop_state(Generic_mission_state):
         # It is easier to control with the keyboard
         self.coordinate_frame = PositionTarget.FRAME_BODY_NED
 
+        self.max_linear_speed = max_linear_speed
+        self.max_angular_speed = max_angular_speed
         self.state_sub = rospy.Subscriber('/cmd_vel', Twist, self.teleop_node_cb)
 
 
@@ -69,13 +73,28 @@ class Teleop_state(Generic_mission_state):
         # Trying to log the altitude
         rospy.loginfo_throttle(5, 'In teleop mode. Altitude: {}'.format(self.z))
 
+    def _check_speeds(self, data):
+        for ax in ['x', 'y', 'z']:
+            linear_speed = getattr(data.linear, ax)
+            angular_speed = getattr(data.angular, ax)
+            if abs(linear_speed) > self.max_linear_speed:
+                setattr(data.linear, ax,
+                        np.sign(linear_speed) * self.max_linear_speed)
+            if abs(angular_speed) > self.max_angular_speed:
+                setattr(data.angular, ax,
+                        np.sign(angular_speed) * self.max_angular_speed)
+        return data
+        
     def teleop_node_cb(self, data):
         # So that the movement is forward
-        self.x_vel = data.linear.y
-        self.y_vel = data.linear.x
+        print self.max_angular_speed
+        checked_data = self._check_speeds(data)
+        
+        self.x_vel = checked_data.linear.y
+        self.y_vel = checked_data.linear.x
         if self.z < 100:  # This is just to try
-            self.z_vel = data.linear.z
-        self.yaw_rate = data.angular.z
+            self.z_vel = checked_data.linear.z
+        self.yaw_rate = checked_data.angular.z
 
 
 def generate_telop_mission(args):
@@ -90,7 +109,10 @@ def generate_telop_mission(args):
     instructions[instruction_cnt] = Take_off_state()
     instruction_cnt += 1
 
-    instructions[instruction_cnt] = Teleop_state(timeout=args.timeout)
+    print('args', args)
+    instructions[instruction_cnt] = Teleop_state(timeout=args.timeout,
+                                                 max_linear_speed=args.linear,
+                                                 max_angular_speed=args.angular)
     instruction_cnt += 1
 
     instructions[instruction_cnt] = Landing_state()
@@ -102,11 +124,13 @@ def generate_telop_mission(args):
 if __name__ == '__main__':
 
     node_name = 'teleop_mission'
-
+    print sys.argv[1:]
     rospy.init_node(node_name, anonymous=True, log_level=rospy.DEBUG)
     parser = argparse.ArgumentParser(description="Teleoperation px4 quadcopter.")
     # This is obtained from the launch file.
     parser.add_argument('-t', '--timeout', type=float, default=30)
+    parser.add_argument('-l', '--linear', type=float, default=4)
+    parser.add_argument('-a', '--angular', type=float, default=2)
     args = parser.parse_args(rospy.myargv(argv=sys.argv)[1:])
     flight_instructions = generate_telop_mission(args)
 
